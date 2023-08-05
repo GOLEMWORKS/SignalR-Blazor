5b37ea2e7c808a0c73464361dc9322b58d11b45b
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-08-06 00:47:57 +0200

    One page clien stuff connection

--- Blazor-Online-Chatting/Program.cs
+++ Blazor-Online-Chatting/Program.cs
@@ -1,4 +1,6 @@
 using Blazor_Online_Chatting.Data;
+using Blazor_Online_Chatting.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -8,6 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] {"application/octet-stream"});
+});
 
 var app = builder.Build();
 
@@ -26,6 +33,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
