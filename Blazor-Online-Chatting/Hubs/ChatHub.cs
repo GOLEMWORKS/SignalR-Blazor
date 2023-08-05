f6d0792d7f29e8c4dce72da1f7606de46951094b
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-08-06 00:10:52 +0200

    SendMessage

---
+++ Blazor-Online-Chatting/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Blazor_Online_Chatting.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage",user, message);
+    }
+}
