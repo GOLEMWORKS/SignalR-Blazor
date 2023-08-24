db181543a0af3aa4bbcd23caa9d8a91c45b58362
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-08-24 15:24:10 +0200

    reconnecting event

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -1,23 +1,30 @@
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace WPFClient
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+    
+namespace WPFClient;
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    HubConnection connection;
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7139/chathub")
+            .WithAutomaticReconnect()
+            .Build();
+
+        connection.Reconnecting += (sender) => 
         {
-            InitializeComponent();
-        }
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Попытка переподключиться...";
+                messages.Items.Add(newMessage);
+            });
+
+            return Task.CompletedTask;
+        };
     }
 }
