173d9de1523309609b9dddd0fd35bea8ec745ff9
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-08-24 15:42:35 +0200

    1:04:23

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -26,5 +26,66 @@ public partial class MainWindow : Window
 
             return Task.CompletedTask;
         };
+
+        connection.Reconnected += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Переподключение успешно!";
+                messages.Items.Clear();
+                messages.Items.Add(newMessage);
+            });
+
+            return Task.CompletedTask;
+        };
+
+        connection.Closed += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Подключение прервано :(";
+                messages.Items.Add(newMessage);
+                openConnection.IsEnabled = true;
+                sendMessage.IsEnabled = false;
+            });
+
+            return Task.CompletedTask;
+        };
+    }
+
+    private async void openConnection_Click(object sender, RoutedEventArgs e)
+    {
+        connection.On<string, string>("Receive message", (user, message) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = $"{user}: {message}";
+                messages.Items.Add(newMessage);
+            });
+        });
+
+        try
+        {
+            await connection.StartAsync();
+            messages.Items.Add("Подключение успешно выполнено!");
+            openConnection.IsEnabled = false;
+            sendMessage.IsEnabled = true;
+        }
+        catch (Exception ex)
+        {
+            messages.Items.Add($"{ex.Message}");
+        }
+    }
+
+    private async void sendMessage_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await connection.InvokeAsync("SendMessage", "WPF Client", messageInput.Text);
+        }
+        catch (Exception ex)
+        {
+            messages.Items.Add($"{ex.Message}");
+        }
     }
 }
